f5eba2c7ab0d833ca589e0d0d763047f475c0bf5
Devin Lu <ludevinl@sfu.ca>
2021-12-15 23:26:14 -0800

    boilerplate code

--- wict/views.py
+++ wict/views.py
@@ -4,4 +4,12 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def index():     
-  return render_template("base.html")
+  return render_template("home.html") 
+
+@views.route('/execs')
+def execs():
+  return render_template("execs.html")
+
+@views.route('/contacts')
+def contacts():
+  return render_template("contacts.html")
