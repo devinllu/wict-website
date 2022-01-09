c6c0e1f31406604a98ff53d605a4ab35b02ce596
Devin Lu <ludevinl@sfu.ca>
2022-01-09 14:47:57 -0800

    updated contacts

--- wict/views.py
+++ wict/views.py
@@ -3,13 +3,9 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 @views.route('/')
-def index():     
+def home():     
   return render_template("home.html") 
 
 @views.route('/execs')
 def execs():
   return render_template("execs.html")
-
-@views.route('/contacts')
-def contacts():
-  return render_template("contacts.html")
