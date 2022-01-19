37bd86c3a7812be7cb188dcf5d8cd9c40e843b3a
Devin Lu <ludevinl@sfu.ca>
2022-01-19 15:32:26 -0800

    fixed browser caching issue

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from wict import create_app
+from flask import url_for
+import os
 
 app = create_app()
+
+# fix for browser caching of CSS or other static files
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 if __name__ == '__main__':
     app.run(debug=True)
